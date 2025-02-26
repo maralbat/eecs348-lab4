32f8b4a6c9d6b9145e7945ce12129b2361cd4cfd
Maral Bat <bmaral1214@gmail.com>
2025-02-26 11:22:32 -0600

    practice 1,2 and additional files

---
+++ functions.js
@@ -0,0 +1,44 @@
+function validate() {
+    var pass1 = document.getElementById("pass1").value;
+    var pass2 = document.getElementById("pass2").value;
+
+
+    if (pass1.length < 8) {
+        alert("The length of the first password is less than 8 letters!")
+    } else if (pass2.length < 8) {
+        alert("The length of the second password is less than 8 letters!")
+    } else if (pass1 != pass2) {
+        alert("Passwords don't match! Try again.")
+    } else {
+        alert("Nice secure password!")
+    }
+}
+
+var picture_index = 0;
+
+function prev() {
+    picture_index = ((picture_index + 5) - 1) % 5
+    var picture_uri = `pictures/${picture_index}.jpeg`
+    document.getElementById("picture").src = picture_uri
+}
+
+function next() {
+    picture_index = (picture_index + 1) % 5
+    var picture_uri = `pictures/${picture_index}.jpeg`
+    document.getElementById("picture").src = picture_uri
+}
+
+function alert_paragraph_color() {
+    var border_R = document.getElementById("border_R").value;
+    var border_G = document.getElementById("border_G").value;
+    var border_B = document.getElementById("border_B").value;
+    var border_width = document.getElementById("border_width").value;
+    var text_R = document.getElementById("text_R").value;
+    var text_G = document.getElementById("text_G").value;
+    var text_B = document.getElementById("text_B").value;
+
+    var tag = document.getElementById("paragraph");
+    tag.style.borderColor = `rgb(${border_R},${border_G},${border_B})`;
+    tag.style.borderWidth = border_width
+    tag.style.color = `rgb(${text_R},${text_G},${text_B})`;
+}
---
+++ practice4.php
@@ -0,0 +1,19 @@
+<?php
+// Outer loop for rows
+for ($i = 1; $i < 7; $i++) {
+    // Inner loop for columns
+    for ($j = 1; $j < 7; $j++) {
+        // Check if it's the first column
+        if ($j == 1) {
+            // Display the product of $i and $j, left-padded to 2 characters
+            echo str_pad($i * $j, 2, " ", STR_PAD_LEFT);
+        } else {
+            // Display the product of $i and $j, left-padded to 4 characters
+            echo str_pad($i * $j, 4, " ", STR_PAD_LEFT);
+        }
+    }
+    // Move to the next line after each row is printed
+    echo "\n";
+}
+
+?>
